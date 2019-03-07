bd0fe624e9602cef9d0fdd2ec5ed108aae02d2c8
Romain Aubert <rpsa@pm.me>
2019-03-07 22:19:05 +0000

    refactor scoreboard controller

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,31 +1,38 @@
 class ScoreboardsController < ApplicationController
-  before_action :my_followers, only: [:index]
+  before_action :scoreboard, only: [:index]
   def index
   end
 
   private
 
+  def scoreboard
+    @users = User.all
+    @user = current_user
+    following_id = follows(@user)
+    following = find_user_by_id(following_id)
+    scores = scores(following)
+    scores << @user.score
+    @sort_scores = sort_scores(scores)
+  end
+
+  # find out who follow
+  def follows(user)
+    user.following_relationships.map(&:following_id)
+  end
+
+  # find user by id
   def find_user_by_id(user_id)
     @users.find(user_id)
   end
 
-  def my_followers
-    @users = User.all
-    @user = current_user
-    @follows = @user.following_relationships.map { |following_relationship| find_user_by_id(following_relationship.following_id) }
-    @scores = scores(@follows)
-    @scores << @user.score
-    @sort_scores = sort_scores(@scores)
-  end
-
   # build an array of scores
   def scores(follows)
-    follows.map { |follow| follow.score }
+    follows.map(&:score)
   end
 
   # sort the array of scores
   def sort_scores(scores)
-    scores.sort_by { |score| -score }
+    scores.sort.reverse
   end
 end
 
