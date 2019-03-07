7bf24129ea46d5d29be1aed1351b9befc1c9c8d6
Romain Aubert <rpsa@pm.me>
2019-03-07 17:02:36 +0000

    add score to users

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,4 +1,18 @@
 class ScoreboardsController < ApplicationController
+  before_action :my_followers, only: [:index]
   def index
+
+    @users = User.all
+    raise
   end
+#
+
+private
+
+def my_followers
+   @user = current_user
+   @followers = @user.following_relationships
+
+end
+# a method for a user to see all his followers should be a where
 end
