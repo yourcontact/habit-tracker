1cab184a13171fcafaa574975d12ba21a2cef547
Romain Aubert <rpsa@pm.me>
2019-03-07 21:20:46 +0000

    build score board listing score of users the current user follows, in descending order

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -5,10 +5,29 @@ class ScoreboardsController < ApplicationController
 
   private
 
+  def find_user_by_id(user_id)
+    @users.find(user_id)
+  end
+
   def my_followers
     @users = User.all
     @user = current_user
-    @follows = @user.following_relationships.map { |following_relationship| @users.find(following_relationship.following_id) }
+    @follows = @user.following_relationships.map { |following_relationship| find_user_by_id(following_relationship.following_id) }
+    @scores = scores(@follows)
+    @scores << @user.score
+    @sort_scores = sort_scores(@scores)
+  end
+
+  # build an array of scores
+  def scores(follows)
+    follows.map { |follow| follow.score }
+  end
+
+  # sort the array of scores
+  def sort_scores(scores)
+    scores.sort_by { |score| -score }
   end
 end
+
+
 
