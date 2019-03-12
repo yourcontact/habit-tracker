6bfbf7d408de970171e2e98ba0346896b84b070c
Romain Aubert <rpsa@pm.me>
2019-03-12 10:51:23 +0000

    kick off work on search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  #skip_before_action :authenticate_user!, only: :index
   before_action :set_user, only: [:follow, :unfollow]
 
   def index
-    @users = User.all.where.not(id: current_user.id)
+    @users = User.where.not(id: current_user.id)
   end
 
   def follow
