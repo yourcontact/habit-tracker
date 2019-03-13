3187a5861b45cc996bf4cc3289398f968ef200ef
joao costa <joaofrancisco0099@hotmail.com>
2019-03-13 12:03:42 +0000

    homepage cards responsive for mobile and add footer to homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -19,6 +20,16 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(current_user)
     habits_path
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "authentication"
+    else
+      "application"
+    end
   end
 
 end
