775891fc5160c2789ec81841f4929e610d991f10
joao costa <joaofrancisco0099@hotmail.com>
2019-03-13 10:16:01 +0000

    redirect signed in user to habit list instead of homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   before_action :skip_footer, only: [:home]
+  before_action :check_signed_in
+
+  def check_signed_in
+    redirect_to habits_path if signed_in?
+  end
 
 
   def home
