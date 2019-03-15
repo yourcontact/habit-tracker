26820d17f4258811e608fb8d3b15fabc8366cd26
joao costa <joaofrancisco0099@hotmail.com>
2019-03-15 12:07:17 +0000

    improve seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,57 +6,25 @@ Unit.destroy_all
 Follow.destroy_all
 
 #create 10 users
-user = User.new(username: "rrrrui", email: "braguaboy@gmail.com", password: "123456", score: "234")
-user.remote_photo_url = 'http://2.bp.blogspot.com/-PGA8jn3WdMU/Uc-IgtfXu9I/AAAAAAAAB80/jg9kc8H7WW0/s324/Cristiano+Ronaldo+Profile+And+Biography.jpg'
-user.save
 
 user = User.new(username: "kevinfromamerica", email: "kevinfromamerica@gmail.com", password: "123456", score: "23")
-user.remote_photo_url = 'http://2.bp.blogspot.com/-PGA8jn3WdMU/Uc-IgtfXu9I/AAAAAAAAB80/jg9kc8H7WW0/s324/Cristiano+Ronaldo+Profile+And+Biography.jpg'
-user.save
-
-user = User.new(username: "antonio", email: "joao@gmail.com", password: "123456", score: "235")
-user.remote_photo_url = 'http://2.bp.blogspot.com/-PGA8jn3WdMU/Uc-IgtfXu9I/AAAAAAAAB80/jg9kc8H7WW0/s324/Cristiano+Ronaldo+Profile+And+Biography.jpg'
-user.save
-
-user = User.new(username: "footinvest", email: "manu@gmail.com", password: "123456", score: "12")
-user.remote_photo_url = 'http://2.bp.blogspot.com/-PGA8jn3WdMU/Uc-IgtfXu9I/AAAAAAAAB80/jg9kc8H7WW0/s324/Cristiano+Ronaldo+Profile+And+Biography.jpg'
+user.remote_photo_url = 'https://avatars2.githubusercontent.com/u/46444582?v=4'
 user.save
 
 user = User.new(username: "Porto4ever", email: "beneficasucks@gmail.com", password: "123456", score: "321")
-user.remote_photo_url = 'http://2.bp.blogspot.com/-PGA8jn3WdMU/Uc-IgtfXu9I/AAAAAAAAB80/jg9kc8H7WW0/s324/Cristiano+Ronaldo+Profile+And+Biography.jpg'
+user.remote_photo_url = 'https://avatars0.githubusercontent.com/u/45975135?v=4'
 user.save
 
-user = User.new(username: "andre", email: "andre@gmail.com", password: "123456", score: "153")
-user.remote_photo_url = 'http://2.bp.blogspot.com/-PGA8jn3WdMU/Uc-IgtfXu9I/AAAAAAAAB80/jg9kc8H7WW0/s324/Cristiano+Ronaldo+Profile+And+Biography.jpg'
+user = User.new(username: "miguelin", email: "miguel@gmail.com", password: "123456", score: "153")
+user.remote_photo_url = 'https://avatars3.githubusercontent.com/u/31628490?v=4'
 user.save
 
-user = User.new(username: "patrick", email: "patrick@gmail.com", password: "123456", score: "76")
-user.remote_photo_url = 'http://2.bp.blogspot.com/-PGA8jn3WdMU/Uc-IgtfXu9I/AAAAAAAAB80/jg9kc8H7WW0/s324/Cristiano+Ronaldo+Profile+And+Biography.jpg'
+user = User.new(username: "businessman", email: "paulo@gmail.com", password: "123456", score: "98")
+user.remote_photo_url = 'https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/vnlkdtuj6n4g5osvdjzp.jpg'
 user.save
 
-user = User.new(username: "ricardobrazil", email: "ricardobrazil@gmail.com", password: "123456", score: "89")
-user.remote_photo_url = 'http://2.bp.blogspot.com/-PGA8jn3WdMU/Uc-IgtfXu9I/AAAAAAAAB80/jg9kc8H7WW0/s324/Cristiano+Ronaldo+Profile+And+Biography.jpg'
-user.save
 
-user = User.new(username: "anonymous", email: "anonymous@gmail.com", password: "123456", score: "3")
-user.remote_photo_url = 'http://2.bp.blogspot.com/-PGA8jn3WdMU/Uc-IgtfXu9I/AAAAAAAAB80/jg9kc8H7WW0/s324/Cristiano+Ronaldo+Profile+And+Biography.jpg'
-user.save
 
-user = User.new(username: "michel", email: "michel@gmail.com", password: "123456", score: "32")
-user.remote_photo_url = 'http://2.bp.blogspot.com/-PGA8jn3WdMU/Uc-IgtfXu9I/AAAAAAAAB80/jg9kc8H7WW0/s324/Cristiano+Ronaldo+Profile+And+Biography.jpg'
-user.save
-
-user = User.new(username: "pedro", email: "pedro@gmail.com", password: "123456", score: "133")
-user.remote_photo_url = 'http://2.bp.blogspot.com/-PGA8jn3WdMU/Uc-IgtfXu9I/AAAAAAAAB80/jg9kc8H7WW0/s324/Cristiano+Ronaldo+Profile+And+Biography.jpg'
-user.save
-
-user = User.new(username: "michael", email: "michael@gmail.com", password: "123456", score: "145")
-user.remote_photo_url = 'http://2.bp.blogspot.com/-PGA8jn3WdMU/Uc-IgtfXu9I/AAAAAAAAB80/jg9kc8H7WW0/s324/Cristiano+Ronaldo+Profile+And+Biography.jpg'
-user.save
-
-user = User.new(username: "yesman", email: "yesman@gmail.com", password: "123456", score: "98")
-user.remote_photo_url = 'http://2.bp.blogspot.com/-PGA8jn3WdMU/Uc-IgtfXu9I/AAAAAAAAB80/jg9kc8H7WW0/s324/Cristiano+Ronaldo+Profile+And+Biography.jpg'
-user.save
 
 #create minute and L units
 unit = Unit.new(name: 'Minutes', weight: 1)
